542b409c1cb91b0ecde4661098605113c2be5682
Angel <angelvf12@educastur.es>
2024-11-26 13:56:30 +0100

    Clases Servicios actualizadas

--- tarea3dwesangel/src/main/java/com/Angelvf3839/tarea3dwesangel/servicios/ServiciosEjemplar.java
+++ tarea3dwesangel/src/main/java/com/Angelvf3839/tarea3dwesangel/servicios/ServiciosEjemplar.java
@@ -1,9 +1,14 @@
 package com.Angelvf3839.tarea3dwesangel.servicios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.Angelvf3839.tarea3dwesangel.repositorios.PlantaRepository;
 
 @Service
 public class ServiciosEjemplar {
 
-	
+    @Autowired
+    PlantaRepository plantarepo;  
+
 }
